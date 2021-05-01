c38ccb3e452a776f98cb015c43aa13345004e418
Josias Aurel <ndjosiasaurel@gmail.com>
2021-05-01 21:10:21 +0000

    react img

--- cartoonizer.py
+++ cartoonizer.py
@@ -6,7 +6,8 @@ st.write("Convert your images into cartoon photos")
 
 image = st.file_uploader("Import Image", type=["png", "jpeg"])
 if image is not None:
-    sketch_gray, sketch_color = cv2.pencilSketch(image, sigma_s=60, sigma_r=0.07, shade_factor=0.05)
+    image_ = cv2.imread(image)
+    sketch_gray, sketch_color = cv2.pencilSketch(image_, sigma_s=60, sigma_r=0.07, shade_factor=0.05)
     st.image(sketch_gray)
     st.image(sketch_color)
 
