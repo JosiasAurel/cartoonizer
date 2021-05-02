9cfb936d1a5497b2ac7f8c9f7893fc377848e705
Josias Aurel <ndjosiasaurel@gmail.com>
2021-05-02 10:49:23 +0100

    Update cartoonizer.py

--- cartoonizer.py
+++ cartoonizer.py
@@ -10,7 +10,7 @@ image = st.file_uploader("Import Image", type=["png", "jpeg", "jpg"])
 if image is not None:
     # print(image.read())
     image_ = np.array(Image.open(image))
-    sketch_gray, sketch_color = cv2.pencilSketch(image_, sigma_s=60, sigma_r=0.07, shade_factor=0.02)
+    sketch_gray, sketch_color = cv2.pencilSketch(image_, sigma_s=60, sigma_r=0.07, shade_factor=0.05)
     st.image(sketch_gray)
     st.image(sketch_color)
     st.balloons()
